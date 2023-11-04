c4555c6928b65b3cc9e4fb4075827a615abd9d5c
shemarlundy <shemarlund@gmail.com>
2023-11-03 22:54:37 -0500

    added https://june-platforms.web.app to cors

--- src/main/java/com/June/CourierNetwork/Auth/SecurityConfig.java
+++ src/main/java/com/June/CourierNetwork/Auth/SecurityConfig.java
@@ -3,9 +3,7 @@ package com.June.CourierNetwork.Auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,17 +12,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 import static com.June.CourierNetwork.Enum.Permission.*;
 import static com.June.CourierNetwork.Enum.Role.ADMIN;
 import static com.June.CourierNetwork.Enum.Role.MANAGER;
 import static org.springframework.http.HttpMethod.*;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -39,13 +31,10 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(withDefaults())
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers(
-                        OPTIONS,
-                        "/**",
                         "/api/v1/auth/**",
                         "/api/v1/user/**",
                         "/api/v1/delivery/details/**",
@@ -97,19 +86,4 @@ public class SecurityConfig  {
 
         return http.build();
     }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Apply this to all paths
-
-        return source;
-    }
-
 }
