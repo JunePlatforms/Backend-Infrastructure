629f697f2334685ccc57150fb2dcc01cd7e0afc0
Shemar Lundy <50093436+SRKING96@users.noreply.github.com>
2023-11-12 09:26:42 -0500

    cors

--- src/main/java/com/June/CourierNetwork/Controller/AdminController.java
+++ src/main/java/com/June/CourierNetwork/Controller/AdminController.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "https://june-platforms.web.app")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/admin")
 @PreAuthorize("hasRole('ADMIN')")
