9ed8b192771e091024342c91d78d357f14373061
Shemar Lundy <50093436+SRKING96@users.noreply.github.com>
2023-11-12 09:30:14 -0500

    cors

--- src/main/java/com/June/CourierNetwork/Controller/TransactionDetailsController.java
+++ src/main/java/com/June/CourierNetwork/Controller/TransactionDetailsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "https://june-platforms.web.app")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/transaction/details")
