fb8ffe0e2b073e51d51a6107f06de267fd8e398c
Shemar Lundy <50093436+SRKING96@users.noreply.github.com>
2023-11-20 22:32:41 -0500

    added welcome email

--- src/main/java/com/June/CourierNetwork/Controller/AdminController.java
+++ src/main/java/com/June/CourierNetwork/Controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://june-platforms.web.app")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
--- src/main/java/com/June/CourierNetwork/Controller/TransactionDetailsController.java
+++ src/main/java/com/June/CourierNetwork/Controller/TransactionDetailsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://june-platforms.web.app")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/transaction/details")
